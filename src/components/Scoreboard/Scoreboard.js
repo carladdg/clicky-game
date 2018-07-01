33dcd17807fadb0be2ac2675f3de3ebd99c16a75
Carla Garcia <cdgarcia95@gmail.com>
2018-07-01 15:30:42 -0500

    Made sticky scoreboard smoother and responsive

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -3,7 +3,8 @@ import "./Scoreboard.css";
 
 class Scoreboard extends Component {
     state = {
-        sticky: false
+        sticky: false,
+        stickySpacerHeight: 0
     }
 
     componentDidMount = () => {
@@ -16,26 +17,31 @@ class Scoreboard extends Component {
 
     handleScroll = () => {
         const pageY = window.scrollY;
-        console.log(pageY);
-        console.log(this.scoreboardElement.offsetTop);
-        console.log(this.scoreboardElement.offsetHeight);
-        console.log(this.scoreboardElement.getBoundingClientRect().bottom);
-        if (this.scoreboardElement.getBoundingClientRect().bottom <= 0) {
-            this.setState({ sticky: true })
-        } else if (pageY === 0) {
+        const headerHeight = 300;
+        const elementHeightWithMargin = this.scoreboardElement.offsetHeight + 30;
+
+        if (pageY >= headerHeight) {
+            this.setState({ 
+                sticky: true,
+                stickySpacerHeight: elementHeightWithMargin
+            })
+        } else {
             this.setState({ sticky: false })
         }
     }
 
     render = () => (
-        <div 
-            className={`scoreboard text-center sticky-${this.state.sticky}`} 
-            ref={(scoreboardElement => this.scoreboardElement = scoreboardElement)} 
-            onScroll={this.handleScroll}
-        >
-            <h4>Click on an image to earn points, but don't click on any more than once!</h4>
-            <h4 className="scores">Current Score: {this.props.currentScore} | Top Score: {this.props.topScore}</h4>
-        </div>
+        <React.Fragment>
+            <div 
+                className={`scoreboard text-center sticky-${this.state.sticky}`} 
+                ref={(scoreboardElement => this.scoreboardElement = scoreboardElement)} 
+                onScroll={this.handleScroll}
+            >
+                <h4>Click on an image to earn points, but don't click on any more than once!</h4>
+                <h4 className="scores">Current Score: {this.props.currentScore} | Top Score: {this.props.topScore}</h4>
+            </div>
+            {this.state.sticky && <div style={{height: this.state.stickySpacerHeight}}></div>}
+        </React.Fragment>
     )
 }
 
